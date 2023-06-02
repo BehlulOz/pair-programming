b3a28a30a77a4fd0c08ecfa92343c88e756107c9
Behlul Ozkul <febehlul@hotmail.com>
2023-06-02 10:22:59 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import React from "react";
+import { BattlePage } from "./pages/BattlePage/BattlePage";
+import { HomePage } from "./pages/HomePage/HomePage";
+import { Navigation } from "./components/Navigation/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+          <header className="App-header">
+            <Navigation />
+          </header>
+          <Routes>
+            <Route path="/" element={<HomePage/>} />
+            <Route path="/battle" element={<BattlePage/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
   );
 }
 
